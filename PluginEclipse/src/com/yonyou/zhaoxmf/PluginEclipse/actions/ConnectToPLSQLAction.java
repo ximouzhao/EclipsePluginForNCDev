a16d010a1628a6f300000bd8691aa10a4f6a7edd
zhaoxmf <zhaoxmf@yonyou.com>
2017-09-18 21:55:56 +0800

    修改类名
    Change-Id: I292616097a6dfa7aa5bd45ba83a76dd17ed2b5fc
    Signed-off-by: zhaoxmf <zhaoxmf@yonyou.com>

--- PluginEclipse/src/com/yonyou/zhaoxmf/PluginEclipse/actions/ConnectToPLSQLAction.java
+++ PluginEclipse/src/com/yonyou/zhaoxmf/PluginEclipse/actions/ConnectToPLSQLAction.java
@@ -1,17 +1,26 @@
 package com.yonyou.zhaoxmf.PluginEclipse.actions;
 
+import java.io.IOException;
+
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
-public class ConnectToPLSQL implements IWorkbenchWindowActionDelegate {
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+
+public class ConnectToPLSQLAction implements IWorkbenchWindowActionDelegate {
 
 	private IWorkbenchWindow window;
 	@Override
 	public void run(IAction action) {
-		// TODO Auto-generated method stub
-
+		try {
+			Runtime.getRuntime().exec("\"C:\\Program Files\\PLSQL Developer\\plsqldev.exe\" userid=WLX_SHYS_201797/1@20.10.129.63:1521/orcl");
+		} catch (IOException e) {
+			MessageDialog.openError(window.getShell(), "ERROR", e.getStackTrace().toString());
+			e.printStackTrace();
+		}
 	}
 
 	@Override
